82e59096dabbe3fdccc13fb73bd2e408256fd5ca
Vansh Khanna <khanna57@ecegrid-thin1.ecn.purdue.edu>
2022-08-31 15:04:54 -0400

    insert function update

--- hash_list.cpp
+++ hash_list.cpp
@@ -16,13 +16,29 @@ hash_list &hash_list::operator=(const hash_list &other)
 void hash_list::insert(int key, float value)
 {
     node* temp = head;
+    newNode = malloc(sizeof(node*));
+    newNode->data = value;
+    newNode->key = key;
+   bool dup = false;
     while(temp != NULL)
     {
         cout << temp->key;
     }
-   
-//check if node exists
+    node* current = head;
 
+    while(current != NULL){
+        if(current->key == key){
+            current->value = value;
+            dup = true;
+            return current;
+        }
+    }
+    while(current->next != NULL){
+          current = current->next;
+    }
+    current->next = newNode; 
+//check if node exists
+    
 //no node there
 
 //node there
@@ -36,5 +52,14 @@ newNode->key = key;
 newNode->value=value;
 newNode->next=NULL;
 return newNode;
+}
+
+void hash_list::remove(int key){
+    
+    node* temp1 = head;
+    node* temp2 = NULL;
+    
+
+    return false;
 }
 
